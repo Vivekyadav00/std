8e097d5be166543c28a5d56ff21cc0f2360f06ea
Vivekyadav00 <166670631+Vivekyadav00@users.noreply.github.com>
2024-04-28 11:15:44 -0500

    Update scripts1.js

--- scripts1.js
+++ scripts1.js
@@ -1,5 +1,5 @@
 // Add your API endpoint here
-var API_ENDPOINT = "API_ENDPOIND_PASTE_HERE";
+var API_ENDPOINT = "https://tigrmjrrt7.execute-api.us-east-1.amazonaws.com/Studentgrades";
 
 // AJAX POST request to save student data
 document.getElementById("savestudent").onclick = function(){
